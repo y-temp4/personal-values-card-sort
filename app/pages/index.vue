deebed6ef6afd3a694b695f227211f5b2507f361
y-temp4 <y.temp4@gmail.com>
2021-02-25 18:19:21 +0900

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <h1>自己省察テスト</h1>
+    <div>Personal Values Card Sort</div>
+    <p>
+      あなたが大切に思う価値観を整理してみましょう。この診断は時間制限などありませんので、ゆっくり進めていただいて大丈夫です。
+    </p>
+    <h2>診断の流れ</h2>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>ステップ 1</v-list-item-title>
+        <v-list-item-subtitle
+          >自分が重要にしている価値観を選択して下さい。数は複数選択可能です。</v-list-item-subtitle
+        >
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>ステップ 2</v-list-item-title>
+        <v-list-item-subtitle
+          >選んだ価値観を 10 個に絞って下さい。</v-list-item-subtitle
+        >
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>ステップ 3</v-list-item-title>
+        <v-list-item-subtitle
+          >10 個の価値観の順位付けをして下さい。</v-list-item-subtitle
+        >
+      </v-list-item-content>
+    </v-list-item>
+    <v-btn color="primary" class="font-weight-bold" x-large
+      >診断をはじめる</v-btn
+    >
+  </main>
+</template>
+
+<script>
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+import { NuxtConfig } from '@nuxt/types'
+import colors from 'vuetify/es5/util/colors'
+
+const config: NuxtConfig = {
+  srcDir: 'app',
+  head: {
+    titleTemplate: '%s - 自己省察テスト',
+    title: '自己省察テスト',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  css: [],
+  plugins: [],
+  components: true,
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+  modules: [],
+  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      // dark: true,
+      themes: {
+        primary: colors.blue.darken2,
+        accent: colors.grey.darken3,
+        secondary: colors.amber.darken3,
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3,
+      },
+    },
+  },
+  build: {},
+}
+
+export default config
