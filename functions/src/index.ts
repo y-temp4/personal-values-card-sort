bb0c58af0175f8b22b7dbe6c852d691ea9c3c5fc
y-temp4 <y.temp4@gmail.com>
2021-02-27 17:20:58 +0900

    Fix for vercel

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,17 @@
-import * as functions from 'firebase-functions'
-const { Nuxt } = require('nuxt')
+// import * as functions from 'firebase-functions'
+// const { Nuxt } = require('nuxt')
 
-const config = require('./nuxt.config.ts')
+// const config = require('./nuxt.config.ts')
 
-const nuxt = new Nuxt({
-  ...config,
-  dev: false,
-  buildDir: 'ssr',
-})
+// const nuxt = new Nuxt({
+//   ...config,
+//   dev: false,
+//   buildDir: 'ssr',
+// })
 
-exports.ssr = functions
-  .runWith({ memory: '512MB' })
-  .https.onRequest(async (req, res) => {
-    await nuxt.ready()
-    return nuxt.render(req, res)
-  })
+// exports.ssr = functions
+//   .runWith({ memory: '512MB' })
+//   .https.onRequest(async (req, res) => {
+//     await nuxt.ready()
+//     return nuxt.render(req, res)
+//   })
