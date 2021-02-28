f14f282a7d3ac47cf4f0e07e9fdd729e3186591d
y-temp4 <y.temp4@gmail.com>
2021-02-28 15:23:06 +0900

    Add ogp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,34 @@
 import { NuxtConfig } from '@nuxt/types'
 
 const isDev = process.env.NODE_ENV === 'development'
+const ogImage = 'https://pvcs.y-temp4.com/ogp.png'
+const title = '自己省察テスト'
+const description = '自己省察テストを通じて自己理解を深めましょう。'
 
 const config: NuxtConfig = {
   srcDir: 'app',
   head: {
-    titleTemplate: '%s - 自己省察テスト',
-    title: '自己省察テスト',
+    titleTemplate: `%s - ${title}`,
+    title,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: description },
+      { hid: 'og:title', property: 'og:title', content: title },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: description,
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:image', property: 'og:image', content: ogImage },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@y_temp4' },
+      { hid: 'twitter:image', name: 'twitter:image', content: ogImage },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
